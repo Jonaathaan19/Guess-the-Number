6935f132c30d39febe54cb4db749b9a7eb429707
Jonaathaan19 <jonathanlp19@gmail.com>
2022-06-29 22:35:07 -0500

    Learn how to handle click events and select elements

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// DOM Manipulation
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+//Select score and current number
+
+// document.querySelector('.number').textContent =13;
+// document.querySelector('.score').textContent = 10;
+
+//Read value from the input field
+// document.querySelector('.guess').value = 22;
+
+//Handling click events
+document.querySelector('.check').addEventListener('click', () => {
+    console.log('Clicked');
+})
