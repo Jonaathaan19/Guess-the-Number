74c357c4cd40b0d6f09d9e1634d38349e8af2988
Jonaathaan19 <jonathanlp19@gmail.com>
2022-07-01 10:25:26 -0500

    Implement game logic

--- script.js
+++ script.js
@@ -13,11 +13,46 @@
 //Read value from the input field
 // document.querySelector('.guess').value = 22;
 
+//Varibles
+
+const message = document.querySelector('.message');
+const scoreDiv = document.querySelector('.score');
+const number = document.querySelector('.number');
+let score = 20;
+
+//Implementation of game logic
+
+//Define secret number
+const secretNumber = Math.trunc(Math.random()*20+1);
+
 //Handling click events
 document.querySelector('.check').addEventListener('click', () => {
     //Save the guess value
-    const guess = document.querySelector('.guess').value;
+    const guess = Number(document.querySelector('.guess').value);
 
-    
+    //Check if the guess is empty
+    if(!guess) {
+        message.textContent = 'Please enter a number ⛔';
+    }
+    //Checks if the number is correct
+    else if(guess === secretNumber) {
+        message.textContent = 'Correct Number 🎊';
+        number.textContent = secretNumber;
+    }
+     //Checks if the number is wrong
+    else if (guess < secretNumber || guess > secretNumber) {
+
+        //If score is 0, game over, else contiues the game until correct or score reaches 0.
+        if(score > 0) {
+            message.textContent = 'Try again 🚫';
+            score--;
+            scoreDiv.textContent = score;
+        }
+
+        else {
+            message.textContent = 'Game Over ⛔';
+            number.textContent = secretNumber;
+        }
+    }
 
 })
